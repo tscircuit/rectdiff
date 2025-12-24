35b4db768df450f143250dfbf79955aea28efb86
0hm☘️ <git8ankanroy@gmail.com>
2025-12-24 15:59:01 +0530

    WIP

--- tests/board-outline.test.ts
+++ tests/board-outline.test.ts
@@ -11,8 +11,7 @@ test("board outline snapshot", async () => {
   // Run to completion
   solver.solve()
 
-  const viz =
-    solver.rectDiffGridSolverPipeline!.rectDiffSeedingSolver!.visualize()
+  const viz = solver.finalVisualize()
   const svg = getSvgFromGraphicsObject(viz)
 
   await expect(svg).toMatchSvgSnapshot(import.meta.path)
