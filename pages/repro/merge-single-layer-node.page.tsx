08f22bb51c3e57fa07703af385a2d1dc7f8f8bf2
0hm☘️ <git8ankanroy@gmail.com>
2026-04-15 08:55:43 +0530

    add fixture

---
+++ pages/repro/merge-single-layer-node.page.tsx
@@ -0,0 +1,21 @@
+import simpleRouteJson from "tests/repro/merge-single-layer-node/merge-single-layer-node.json"
+import { RectDiffPipeline } from "lib/RectDiffPipeline"
+import { useMemo } from "react"
+import { SolverDebugger3d } from "components/SolverDebugger3d"
+
+export default () => {
+  const solver = useMemo(
+    () =>
+      new RectDiffPipeline({
+        simpleRouteJson: simpleRouteJson.simple_route_json,
+      }),
+    [],
+  )
+
+  return (
+    <SolverDebugger3d
+      solver={solver}
+      simpleRouteJson={simpleRouteJson.simple_route_json}
+    />
+  )
+}
