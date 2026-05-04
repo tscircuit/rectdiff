596c968f985ffec7052e17f234a6bac37ce28380
0hm☘️ <git8ankanroy@gmail.com>
2026-05-04 20:19:40 +0530

    types

--- global.d.ts
+++ global.d.ts
@@ -2,3 +2,10 @@ declare module "*.json" {
   const value: any
   export default value
 }
+
+declare module "@tscircuit/autorouting-dataset-01" {
+  import type { SimpleRouteJson } from "./lib/types/srj-types"
+
+  const dataset: Record<string, SimpleRouteJson>
+  export = dataset
+}
