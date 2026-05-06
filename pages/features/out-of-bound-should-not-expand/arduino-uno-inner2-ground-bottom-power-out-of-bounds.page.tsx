dde3c5011c6bec572274f89b6fe6d922c8b68e91
0hm☘️ <109351887+0hmX@users.noreply.github.com>
2026-05-06 13:59:21 +0530

    relocated and add new aurdino repro (#125)

---
+++ pages/features/out-of-bound-should-not-expand/arduino-uno-inner2-ground-bottom-power-out-of-bounds.page.tsx
@@ -0,0 +1,10 @@
+import simpleRouteJson from "../../../test-assets/arduino-uno-inner2-ground-bottom-power-out-of-bounds.json"
+import { RectDiffPipeline } from "../../../lib/RectDiffPipeline"
+import { useMemo } from "react"
+import { SolverDebugger3d } from "../../../components/SolverDebugger3d"
+
+export default () => {
+  const solver = useMemo(() => new RectDiffPipeline({ simpleRouteJson }), [])
+
+  return <SolverDebugger3d solver={solver} simpleRouteJson={simpleRouteJson} />
+}
