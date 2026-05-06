cbd1acaa9fcd5346be3bd7ba4755fa8ea728cfbb
0hm☘️ <git8ankanroy@gmail.com>
2026-05-06 13:57:52 +0530

    relocated and add new aurdino repro

---
+++ pages/features/out-of-bound-should-not-expand/simplified-out-of-bounds-example.page.tsx
@@ -0,0 +1,10 @@
+import simpleRouteJson from "../../../test-assets/simplified-out-of-bounds-example.json"
+import { RectDiffPipeline } from "../../../lib/RectDiffPipeline"
+import { useMemo } from "react"
+import { SolverDebugger3d } from "../../../components/SolverDebugger3d"
+
+export default () => {
+  const solver = useMemo(() => new RectDiffPipeline({ simpleRouteJson }), [])
+
+  return <SolverDebugger3d solver={solver} simpleRouteJson={simpleRouteJson} />
+}
