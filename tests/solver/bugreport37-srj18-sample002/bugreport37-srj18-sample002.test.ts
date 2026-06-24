f3337e48b4fc1bb65a0b94cb87fd669e2ae59f9a
0hm☘️ <109351887+0hmX@users.noreply.github.com>
2026-06-24 16:19:45 +0530

    Add srj18 sample002 visual fixture GAPS repo (#135)

---
+++ tests/solver/bugreport37-srj18-sample002/bugreport37-srj18-sample002.test.ts
@@ -0,0 +1,11 @@
+import { expect, test } from "bun:test"
+import { getSvgFromGraphicsObject } from "graphics-debug"
+import { RectDiffPipeline } from "lib/RectDiffPipeline"
+import simpleRouteJson from "./bugreport37-srj18-sample002.json"
+test("bugreport37-srj18-sample002", async () => {
+  const solver = new RectDiffPipeline({ simpleRouteJson })
+  solver.solve()
+  await expect(
+    getSvgFromGraphicsObject(solver.visualize(), { backgroundColor: "white" }),
+  ).toMatchSvgSnapshot(import.meta.path)
+})
